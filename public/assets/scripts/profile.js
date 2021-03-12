13390c0150febe0445922987460857ee88df6a5f
robertogriel <roberto@griel.com.br>
2021-03-12 20:38:01 -0300

    Ready to deploy

--- public/assets/scripts/profile.js
+++ public/assets/scripts/profile.js
@@ -20,7 +20,7 @@ document.querySelectorAll("#app").forEach(page => {
             const addressElement = page.querySelector("#address")
             const districtElement = page.querySelector("#district")
             const cityElement = page.querySelector("#city")
-            const stateElement = page.querySelector("#city")
+            const stateElement = page.querySelector("#state")
 
             const zipcodeElement = page.querySelector("#zipcode")
             const phoneElement = page.querySelector("#phone")
@@ -61,14 +61,117 @@ document.querySelectorAll("#app").forEach(page => {
                     url: `https://viacep.com.br/ws/${value.replace("-", "")}/json/`
                 }).then(({data}) => {
 
+                    let logradouro = '';
+                    let bairro = '';
+                    let cidade = '';
+                    let estado = '';
+
+                    if (data.logradouro) {
+                        logradouro = data.logradouro;
+                        numberElement.focus();
+                    } else {
+                        addressElement.focus();
+                    }
+                    if (data.bairro) {
+                        bairro = data.bairro;
+                    }
+                    if (data.localidade) {
+                        cidade = data.localidade;
+                    }
+                    
+                    switch (data.uf) {
+                        case "AC":
+                            estado = "Acre";
+                            break;
+                        case "AL":
+                            estado = "Alagoas";
+                            break;
+                        case "AP":
+                            estado = "Amapá";
+                            break;
+                        case "AM":
+                            estado = "Amazonas";
+                            break;
+                        case "BA":
+                            estado = "Bahia";
+                            break;
+                        case "CE":
+                            estado = "Ceará";
+                            break;
+                        case "DF":
+                            estado = "Distrito Federal";
+                            break;
+                        case "ES":
+                            estado = "Espírito Santo";
+                            break;
+                        case "GO":
+                            estado = "Goiás";
+                            break;
+                        case "MA":
+                            estado = "Maranhão";
+                            break;
+                        case "MT":
+                            estado = "Mato Grosso";
+                            break;
+                        case "MS":
+                            estado = "Mato Grosso do Sul";
+                            break;
+                        case "MG":
+                            estado = "Minas Gerais";
+                            break;
+                        case "PA":
+                            estado = "Pará";
+                            break;
+                        case "PB":
+                            estado = "Paraíba";
+                            break;
+                        case "PR":
+                            estado = "Paraná";
+                            break;
+                        case "PE":
+                            estado = "Pernambuco";
+                            break;
+                        case "PI":
+                            estado = "Piauí";
+                            break;
+                        case "RJ":
+                            estado = "Rio de Janeiro";
+                            break;
+                        case "RN":
+                            estado = "Rio Grande do Norte";
+                            break;
+                        case "RS":
+                            estado = "Rio Grande do Sul";
+                            break;
+                        case "RO":
+                            estado = "Rondônia";
+                            break;
+                        case "RR":
+                            estado = "Roraima";
+                            break;
+                        case "SC":
+                            estado = "Santa Catarina";
+                            break;
+                        case "SP":
+                            estado = "São Paulo";
+                            break;
+                        case "SE":
+                            estado = "Sergipe";
+                            break;
+                        case "TO":
+                            estado = "Tocantins";
+                            break;
+                    }
+
+                    console.log(estado)
+
                     setFormValues(formElement, {
-                        address: data.logradouro,
-                        district: data.bairro,
-                        city: data.localidade,
-                        state: data.uf,
+                        address: logradouro,
+                        district: bairro,
+                        city: cidade,
+                        state: estado,
                     })
 
-                    numberElement = focus()
 
                 }).catch(error => {
                     
@@ -95,7 +198,10 @@ document.querySelectorAll("#app").forEach(page => {
                         "state": stateElement.value
                     })
                     .then(() => {
-                        showAlert("Sucesso: Cadastro Efetuado com sucesso", "success");
+                        showAlert("Sucesso: Seus dados foram salvos com sucesso", "success");
+                        setTimeout(()=>{
+                            window.location.href = 'index.html';
+                        }, 5000)
                     })
                     .catch((error) => {
                         showAlert("Desculpe algo de errado não está certo, verifique e tente novamente!", "error")
